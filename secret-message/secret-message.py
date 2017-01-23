b2f3e00fa74fde1598a6a65252f04523e629b7ef
Joshua Granville <jgranville@QInformation.com>
2017-01-23 16:22:49 -0500

    Works!

--- secret-message/secret-message.py
+++ secret-message/secret-message.py
@@ -4,9 +4,20 @@ def rename_files():
     # get all file names
     file_list = os.listdir(r"c:\git\udacity-python\secret-message\prank\prank")
     print (file_list)
+    
+    saved_path = os.getcwd()
+    os.chdir (r"c:\git\udacity-python\secret-message\prank\prank")
 
+    to_remove = "0123456789"
+    table = str.maketrans("", "", to_remove)
+    
     #rename each file
     for file in file_list:
-        os.rename(file, file.translate("0123456789"))
+        print("Old Name - "+file)
+        print("New Name - "+file.translate(table))
+        os.rename(file, file.translate(table))
 
+    os.chdir(saved_path)
+    
 rename_files()
+
